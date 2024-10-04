78981607328c018876fb5dc32888a3f0a97ab050
jlinaresb <joselb1993@gmail.com>
2024-10-04 16:17:52 +0200

    Figure 4

--- Figures/Figure4/Figure4_scatter_supp.R
+++ Figures/Figure4/Figure4_scatter_supp.R
@@ -1,33 +1,42 @@
-require(ggpubr)
-s1 = ggscatter(gsva, x = 'ours', y = 'G0.EarlyG1', add = 'reg.line', conf.int = T,
+# Plot scatter plots between signature scores and specific cell cycles signatures
+source("requirements.R")
+
+# Input paths
+cc_signatures_inputpath <- "extdata/signatures/cell_cycle.rds"
+tcga_inputpath <- "data/pp/counts_norm_coad_patients.rds"
+coad_signatures_inputpath <- "extdata/signatures/signatures_coad.rds"
+
+# Load data
+train <- readRDS(tcga_inputpath)
+cc_signatures <- readRDS(cc_signatures_inputpath)
+coad_signatures <- readRDS(coad_signatures_inputpath)
+
+# Define signatures
+signatures <- c(cc_signatures, coad_signatures)
+
+# Calculate signature´s activity
+gsva <- gsva(
+    t(train),
+    signatures,
+    method = "plage",
+    kcdf = "Gaussian")
+gsva <- as.data.frame(t(gsva))
+
+# Plot
+s1 = ggscatter(gsva, x = 'ours', y = 'G1Phase', add = 'reg.line', conf.int = T,
                add.params = list(color = viridis(3)[1], fill = 'lightgray')) +
   stat_cor(method = 'pearson')
-s2 = ggscatter(gsva, x = 'ours', y = 'G1Phase', add = 'reg.line', conf.int = T,
+s2 = ggscatter(gsva, x = 'ours', y = 'M.prophase', add = 'reg.line', conf.int = T,
                add.params = list(color = viridis(3)[1], fill = 'lightgray')) +
   stat_cor(method = 'pearson')
-s3 = ggscatter(gsva, x = 'ours', y = 'G1S.transition', add = 'reg.line', conf.int = T,
+s3 = ggscatter(gsva, x = 'ours', y = 'M.prometaphase', add = 'reg.line', conf.int = T,
                add.params = list(color = viridis(3)[1], fill = 'lightgray')) +
   stat_cor(method = 'pearson')
-s4 = ggscatter(gsva, x = 'ours', y = 'S.phase', add = 'reg.line', conf.int = T,
+s4 = ggscatter(gsva, x = 'ours', y = 'M.metaphase.anaphase', add = 'reg.line', conf.int = T,
                add.params = list(color = viridis(3)[1], fill = 'lightgray')) +
   stat_cor(method = 'pearson')
-s5 = ggscatter(gsva, x = 'ours', y = 'G2.phase', add = 'reg.line', conf.int = T,
-               add.params = list(color = viridis(3)[1], fill = 'lightgray')) +
-  stat_cor(method = 'pearson')
-s6 = ggscatter(gsva, x = 'ours', y = 'G2M.transition', add = 'reg.line', conf.int = T,
-               add.params = list(color = viridis(3)[1], fill = 'lightgray')) +
-  stat_cor(method = 'pearson')
-s7 = ggscatter(gsva, x = 'ours', y = 'M.prophase', add = 'reg.line', conf.int = T,
-               add.params = list(color = viridis(3)[1], fill = 'lightgray')) +
-  stat_cor(method = 'pearson')
-s8 = ggscatter(gsva, x = 'ours', y = 'M.prometaphase', add = 'reg.line', conf.int = T,
-               add.params = list(color = viridis(3)[1], fill = 'lightgray')) +
-  stat_cor(method = 'pearson')
-s9 = ggscatter(gsva, x = 'ours', y = 'M.metaphase.anaphase', add = 'reg.line', conf.int = T,
-               add.params = list(color = viridis(3)[1], fill = 'lightgray')) +
-  stat_cor(method = 'pearson')
-s10 = ggscatter(gsva, x = 'ours', y = 'M.telophase.cytokinesis', add = 'reg.line', conf.int = T,
+s5 = ggscatter(gsva, x = 'ours', y = 'M.telophase.cytokinesis', add = 'reg.line', conf.int = T,
                 add.params = list(color = viridis(3)[1], fill = 'lightgray')) +
   stat_cor(method = 'pearson')
 
-ggarrange(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, nrow = 5, ncol = 2)
+ggarrange(s1, s2, s3, s4, s5, nrow = 3, ncol = 2)
